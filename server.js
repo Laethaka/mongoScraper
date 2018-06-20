6b26592afc6902ad8bd93de6e0ef229c5e871e3b
Jesse <laethaka@gmail.com>
2018-06-20 16:07:21 -0700

    functional

--- server.js
+++ server.js
@@ -1,86 +1,94 @@
 // Dependencies
 var express = require("express");
-var mongojs = require("mongojs");
-// Require request and cheerio. This makes the scraping possible
+// var mongojs = require("mongojs");
 var request = require("request");
 var cheerio = require("cheerio");
+var mongoose = require('mongoose');
+var bodyParser = require("body-parser");
 
 // Initialize Express
 var app = express();
 
-// Database configuration
-var databaseUrl = "steamScraper";
-var collections = ["scrapedSteam"];
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-// Hook mongojs configuration to the db variable
-var db = mongojs(databaseUrl, collections);
-db.on("error", function(error) {
-  console.log("Database Error:", error);
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/steamScraper", {
 });
 
-// Main route (simple Hello World Message)
-app.get("/", function(req, res) {
-  res.send("Hello world");
+var db = require("./models");
+
+app.get("/", function (req, res) {
+  res.send("Server working");
 });
 
-// Retrieve data from the db
-app.get("/all", function(req, res) {
-  // Find all results from the scrapedData collection in the db
-  db.scrapedData.find({}, function(error, found) {
-    // Throw any errors to the console
-    if (error) {
-      console.log(error);
-    }
-    // If there are no errors, send the data to the browser as json
-    else {
-      res.json(found);
-    }
+app.get("/all", function (req, res) {
+  db.Article.find({}).then(function (found) {
+    res.json(found);
   });
 });
 
-// Scrape data from one site and place it into the mongodb db
-app.get("/scrape", function(req, res) {
-
-  // Make a request for the news section of ycombinator
-  request("https://store.steampowered.com/news/?feed=steam_announce", function(error, response, html) {
-    // Load the html body from request into cheerio
-    var $ = cheerio.load(html);
-    // For each element with a "title" class
-    $(".newsPostBlock").each(function(i, element) {
-      // Save the text and href of each link enclosed in the current element
-      var title = $(element).children('.feed').text().split('-')[0];
-      var link = $(element).children(".body").children('a:first-child').attr("href");
-      var summary = $(element).children('.body').text();
-
-    // console.log(title, link, summary)
-
-      // If this found element had both a title and a link
-        if (link.includes('http://store.steampowered.com/app/')) { //NEWS ITEM IS FOR A DISCRETE GAME
-            // Insert the data in the scrapedData db
-            db.scrapedSteam.insert({
-                title: title,
-                link: link,
-                summary: summary
-            },
-            function(err, inserted) {
-                if (err) {
-                // Log the error if one is encountered during the query
-                console.log(err);
-                } else {
-                // Otherwise, log the inserted data
-                console.log(inserted);
-                }
-            });
-        }
+app.get("/post/:id", function (req, res) {
+  db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function (result) {
+      res.json(result);
+    })
+    .catch(function (err) {
+      res.json(err);
     });
-  });
+});
 
-  // Send a "Scrape Complete" message to the browser
+app.post("/post/:id", function (req, res) {
+  db.Note.create(req.body)
+    .then(function (noteInput) {
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: noteInput._id }, { new: true });
+    })
+    .then(function (result) {
+      res.json(result);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+app.get("/scrape", function (req, res) {
+
+  request("https://store.steampowered.com/news/?feed=steam_announce", function (error, response, html) {
+    var $ = cheerio.load(html);
+    $(".newsPostBlock").each(function (i, element) { //LOOPING THROUGH ALL POSTS ON STEAM ANNOUNCEMENTS PAGE
+      var title = $(element).children('.feed').text().split('-')[0].trim();
+      var link = $(element).children(".body").children('a:first-child').attr("href");
+      var summary = $(element).children('.body').text().trim();
+
+      if (link.includes('http://store.steampowered.com/app/')) { //POST IS FOR A DISCRETE GAME
+        db.Article.find({ title: title }, function (err, res) {
+          if (res.length === 0) {//POST NOT YET IN MONGO
+            db.Article.create({
+              title: title,
+              link: link,
+              summary: summary
+            },
+              function (err, inserted) {
+                if (err) {
+                  console.log(err);
+                } else {
+                  console.log('inserted', title);
+                }
+              });
+          } else {
+            console.log('duplicate post scraped!', title)
+          }
+        })
+      } else { //POST IS SOME RANDOM CRAP
+        console.log('rejecting this random crap:', title)
+      };
+    }); //LOOP ENDS
+  });
   res.send("Scrape Complete");
 });
 
 
-// Listen on port 3000
-app.listen(5588, function() {
+app.listen(5588, function () {
   console.log("App running on port 5588!");
 });
